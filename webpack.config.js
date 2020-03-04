7d682014c6e697f57d7148b65b076cdf4b9d6b1b
pvlach <vlach1989@gmail.com>
2020-03-04 12:37:47 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+var path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        sourceMapFilename: "index.js.map",
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader', 'source-map-loader']
+            }, {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            }, {
+                test: /\.css$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader'
+                ],
+            },{
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }, {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-inline-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    externals: {
+        "@gisatcz/ptr-utils": "@gisatcz/ptr-utils",
+        "@gisatcz/ptr-core": "@gisatcz/ptr-core",
+        "@gisatcz/ptr-atoms": "@gisatcz/ptr-atoms",
+        "@gisatcz/ptr-locales": "@gisatcz/ptr-locales",
+        "classnames": "classnames",
+        "prop-types": "prop-types",
+        "lodash": "lodash",
+        "react": "react",
+        "react-dom": "react-dom",
+    }
+};
